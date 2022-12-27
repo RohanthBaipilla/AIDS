d2c468269697e131255f1f212262585733ecd2f3
RohanthBaipilla <107876880+RohanthBaipilla@users.noreply.github.com>
2022-12-27 12:09:10 +0530

    Create Sum2.py

---
+++ LAB/IN-LAB 1/Sum2.py
@@ -0,0 +1,27 @@
+problem=["Over Heating","Battery dying quickly","System crash","viruses","Display not working","Other Problem"]
+print("Hello I came from company services")
+print("May I know your name?")
+name=input()
+print("Thank you", name)
+print("How can I help you",name,"??")
+a=input()
+print("Ok!!!")
+print("I am here to help you explore through your problem of laptop")
+for i in range(0,6):
+    print((i+1),".",problem[i])
+print("Choose your problem")
+num=input()
+dict={
+        "1":"Keep the Laptop on a Hard and Flat SurfaceMost laptops keep in cooling air through their bottoms.",
+        "2":"Your battery drains much faster when it's hot, even when not in use.",
+        "3":"Most PC or laptop crashes are the result of overheating, hardware faulty, corrupted system or driver corruption, etc.",
+        "4":"A computer virus is a type of malware",
+        "5":"check if your laptop's brightness level is set to maximum. If the display is still dim, try restarting the device.",
+        "6":"Go to customer service tell their what is your problem."
+    }
+number=""
+for i in num:
+    number+=dict.get(i,"")+" "
+print(number)
+print("Thank You...!")
+print("I think I had cleared your Doubts")
